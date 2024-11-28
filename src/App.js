01441bb614e77bb1a82e807b9cfa856c9dcf122c
yuvi070 <yuvarajchavan448@gmail.com>
2024-11-28 13:45:03 +0000

    initial Code

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import './App.css'
+
+// These are the lists used in the application. You can move them to any component needed.
+const topicsList = [
+  {
+    id: 'ARTS_AND_CULTURE',
+    displayText: 'Arts and Culture',
+  },
+  {
+    id: 'CAREER_AND_BUSINESS',
+    displayText: 'Career and Business',
+  },
+  {
+    id: 'EDUCATION_AND_LEARNING',
+    displayText: 'Education and Learning',
+  },
+  {
+    id: 'FASHION_AND_BEAUTY',
+    displayText: 'Fashion and Learning',
+  },
+  {
+    id: 'GAMES',
+    displayText: 'Games',
+  },
+]
+
+// Replace your code here
+const App = () => <h1>Hello World</h1>
+
+export default App
